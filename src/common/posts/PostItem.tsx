4332ac85a6f3053e78f9263d44a462bddaad0b0e
Igor Datsenko <datsenko2802@gmail.com>
2022-09-10 14:50:29 +0300

    add new file in posts

--- src/common/posts/PostItem.tsx
+++ src/common/posts/PostItem.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-export const PostItem = () => {
+export const PostItem = (props) => {
+    const { body, id } = props;
+	const postText = useRef
+	const openText = () => {
+		postText.current.classList.toggle("open-text");
+	}
     return (
         <div>
-            
+            <div className="posts__box">
+			<ul className="posts__list">
+				<li className="list__item">
+					<div className="item__title" onClick={() => {
+						openText()}}>Title {id}</div>
+					<div className="item__text" ref={postText}>{body}</div>
+				</li>
+			</ul>
+		</div>
         </div>
     );
 };
